586deaddf979f9dcbed7c3e0ae200cbb007af2de
remigourdon <gourdon.remi@gmail.com>
2019-06-17 15:52:40 -0400

    Pass interface names to polybar in environment

--- gui/polybar/launch.sh
+++ gui/polybar/launch.sh
@@ -6,13 +6,15 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-if type "xrandr"; then
+# Find interfaces
+INTERFACES="$(ip link show | grep -o '^[[:digit:]]: [[:alnum:]]\+' | cut -d' ' -f2)"
+WIRELESS="$(grep '^wlp' <<<"${INTERFACES}" | tail -n1)"
+WIRED="$(grep '^en' <<<"${INTERFACES}" | tail -n1)"
+
+if type "xrandr" &>/dev/null ; then
     for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-        MONITOR=$m polybar --reload top &
-        MONITOR=$m polybar --reload bottom &
+        MONITOR=$m polybar -q --reload top &
+        MONITOR=$m WIRELESS=$WIRELESS WIRED=$WIRED polybar -q --reload bottom &
     done
-else
-    polybar --reload example &
 fi
 
-echo "Polybars launched!"
