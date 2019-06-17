677d834d824ce3e629d013b58b763201ffa4dfb8
remigourdon <gourdon.remi@gmail.com>
2019-06-17 14:48:19 -0400

    Add script for changing wallpaper with i3 binding

--- scripts/lenovo-dock.sh
+++ scripts/lenovo-dock.sh
@@ -9,12 +9,12 @@ if [[ "${MONITORS}" == *"DP-1-1"* ]]; then
     xrandr --output eDP-1 --mode 1920x1080 --pos 0x0 --rotate normal
     xrandr --output DP-1-1 --primary --mode 1920x1080 --pos 1920x0 --rotate normal
     ~/.config/polybar/launch.sh
-    wal -R
+    set-wallpaper.sh -r
 else
     # Undocked
     echo 'Undocked' > /home/remi/lenovo-test.txt
     xrandr --output eDP-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal
     xrandr --output DP-1-1 --off
     ~/.config/polybar/launch.sh
-    wal -R
+    set-wallpaper.sh -r
 fi
---
+++ scripts/set-wallpaper.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+# Set wallpaper and update wal theme
+set -u # Treat unset variables and parameters as errors
+
+#########
+# USAGE #
+#########
+
+usage() {
+    echo "Usage: $(basename "${0}") [OPTIONS] INPUT_IMAGE
+
+Set wallpaper and update wal theme.
+
+OPTIONS:
+    -o    Only set wallpaper
+    -r    Only reload the wallpaper
+    -h    Display this message"
+}
+
+###############
+# DEFINITIONS #
+###############
+
+WAL_IMG="${HOME}/.wallpaper"
+
+###################
+# OPTIONS PARSING #
+###################
+
+SET_THEME=true
+INPUT_IMAGE=""
+
+while getopts 'orh' opts ; do
+    case "${opts}" in
+        o)
+            SET_THEME=false
+            ;;
+        r)
+            feh --bg-scale "${WAL_IMG}"
+            exit 0
+            ;;
+        h)
+            usage
+            exit 1
+            ;;
+        ?)
+            usage
+            exit 1
+            ;;
+    esac
+done
+shift "$((OPTIND - 1))"
+
+################
+# ARGS PARSING #
+################
+
+if [[ ${#} -ne 1 ]] ; then
+    usage
+    exit 1
+else
+    INPUT_IMAGE="${1%$'\n'}"
+fi
+
+##########
+# ACTION #
+##########
+
+cp -f "${INPUT_IMAGE}" "${WAL_IMG}"
+
+if [[ "${SET_THEME}" = true ]] ; then
+    wal -c && wal -q -i "${WAL_IMG}"
+else
+    feh --bg-scale "${WAL_IMG}"
+fi
