7c943f85d78cf67b1208515a6be0a4e1508c16c4
remigourdon <gourdon.remi@gmail.com>
2019-06-17 10:03:10 -0400

    Update polybar launch script for multiple monitors

--- gui/polybar/launch.sh
+++ gui/polybar/launch.sh
@@ -6,8 +6,13 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# Launch bars
-polybar top &
-polybar bottom &
+if type "xrandr"; then
+    for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+        MONITOR=$m polybar --reload top &
+        MONITOR=$m polybar --reload bottom &
+    done
+else
+    polybar --reload example &
+fi
 
 echo "Polybars launched!"
