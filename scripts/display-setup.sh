a7cba5763909ebf0e528b56c57dc748106ee2c7a
remigourdon <gourdon.remi@gmail.com>
2019-09-16 18:03:31 -0400

    Update display configurations for new work setup

--- scripts/display-setup.sh
+++ scripts/display-setup.sh
@@ -4,8 +4,8 @@ MONITORS="$(xrandr --query | grep -o '^[^ ]* connected' | cut -d' ' -f1)"
 
 if [[ "${MONITORS}" == *"DP-1-1"* ]]; then
     # Docked
-    xrandr --output eDP-1 --mode 1920x1080 --pos 0x0 --rotate normal
-    xrandr --output DP-1-1 --primary --mode 1920x1080 --pos 1920x0 --rotate normal
+    xrandr --output DP-1-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal
+    xrandr --output eDP-1 --mode 1920x1080 --pos 1920x0 --rotate normal
     ~/.config/polybar/launch.sh
     set-wallpaper.sh -r
 else
