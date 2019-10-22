a292caae15004406e6c2a1c133bb94a6bc58229e
remigourdon <gourdon.remi@gmail.com>
2019-10-22 11:03:02 -0400

    Update display setup for dual external monitors

--- scripts/display-setup.sh
+++ scripts/display-setup.sh
@@ -4,14 +4,16 @@ MONITORS="$(xrandr --query | grep -o '^[^ ]* connected' | cut -d' ' -f1)"
 
 if [[ "${MONITORS}" == *"DP-1-1"* ]]; then
     # Docked
+    xrandr --output eDP-1 --off
     xrandr --output DP-1-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal
-    xrandr --output eDP-1 --mode 1920x1080 --pos 1920x0 --rotate normal
+    xrandr --output DP-1-2 --mode 1920x1080 --pos 1920x0 --rotate normal
     ~/.config/polybar/launch.sh
     set-wallpaper.sh -r
 else
     # Undocked
     xrandr --output eDP-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal
     xrandr --output DP-1-1 --off
+    xrandr --output DP-1-2 --off
     ~/.config/polybar/launch.sh
     set-wallpaper.sh -r
 fi
