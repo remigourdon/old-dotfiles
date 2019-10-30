cc7cf209e0a845cbfa611ce963749782c409a5b0
remigourdon <gourdon.remi@gmail.com>
2019-10-29 23:22:12 -0400

    Replace custom locking script with i3lock-fancy

--- gui/rofi/scripts/power.sh
+++ gui/rofi/scripts/power.sh
@@ -4,7 +4,7 @@ set -u # Treat unset variables and parameters as errors
 
 LAUNCHER="rofi -dmenu -i -p power -format d"
 
-OPTIONS="Lock system\nReboot system\nPower-off system"
+OPTIONS="Lock system\nReboot system\nPower-off system\nLog out"
 
 OPTION=$(echo -e "${OPTIONS}" | ${LAUNCHER})
 
@@ -13,6 +13,7 @@ if [[ "${#OPTION}" -gt 0 ]] ; then
         1) loginctl lock-session ;;
         2) systemctl reboot ;;
         3) systemctl poweroff ;;
+        4) i3-msg exit ;;
         *) exit 1 ;;
     esac
 fi
