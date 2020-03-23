ea8b2d2c20dec195ea1e37ef5ea5c16f8f545b90
remigourdon <gourdon.remi@gmail.com>
2020-03-23 08:47:24 -0400

    Add home office display setup

--- scripts/display-setup.sh
+++ scripts/display-setup.sh
@@ -9,6 +9,12 @@ if [[ "${MONITORS}" == *"DP-1-1"* ]]; then
     xrandr --output DP-1-2 --mode 1920x1080 --pos 1920x0 --rotate normal
     ~/.config/polybar/launch.sh
     set-wallpaper.sh -r
+elif [[ "${MONITORS}" == *"HDMI-1"* ]] ; then
+    # Home
+    xrandr --output HDMI-1 --mode 1920x1080 --pos 0x0 --rotate normal
+    xrandr --output eDP-1 --primary --mode 1920x1080 --pos 0x1080 --rotate normal
+    ~/.config/polybar/launch.sh
+    set-wallpaper.sh -r
 else
     # Undocked
     xrandr --output eDP-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal
