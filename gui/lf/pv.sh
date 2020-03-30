924bc8e1974d148e68ced6d33346fabdbfbea137
remigourdon <gourdon.remi@gmail.com>
2020-03-30 12:57:45 -0400

    Use aliases in lf previewer script

--- gui/lf/pv.sh
+++ gui/lf/pv.sh
@@ -1,4 +1,6 @@
-#!/bin/bash
+#!/bin/sh
+
+. "${HOME}/.aliases"
 
 case "$1" in
     *.tar*) tar tf "$1" ;;
@@ -6,8 +8,8 @@ case "$1" in
     *.rar) unrar l "$1" ;;
     *.7z) 7z l "$1" ;;
     *.pdf) pdftotext "$1" - ;;
-    *.csv) column -t -s , -n "$1" ;;
-    *.tsv) column -t -s $'\t' -n "$1" ;;
-    *.json) jq '.' "$1" | bat --plain --theme=base16 --color=always -l JSON ;;
+    *.csv) csv "$1" ;;
+    *.tsv) tsv "$1" ;;
+    *.json) json "$1" | bat --plain --theme=base16 --color=always -l JSON ;;
     *) bat --number --theme=base16 --color=always "$1" ;;
 esac
