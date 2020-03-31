8a900b4f9a6d6f5325ab7e32eb2c86a24948628c
remigourdon <gourdon.remi@gmail.com>
2020-03-31 11:52:10 -0400

    Update display setup for home docking with one monitor

--- scripts/display-setup.sh
+++ scripts/display-setup.sh
@@ -2,16 +2,16 @@
 
 MONITORS="$(xrandr --query | grep -o '^[^ ]* connected' | cut -d' ' -f1)"
 
-if [[ "${MONITORS}" == *"DP-1-1"* ]]; then
+if [[ "${MONITORS}" == *"DP-1-1"* && "${MONITORS}" == *"DP-1-2"* ]]; then
     # Docked
     xrandr --output eDP-1 --off
     xrandr --output DP-1-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal
     xrandr --output DP-1-2 --mode 1920x1080 --pos 1920x0 --rotate normal
     ~/.config/polybar/launch.sh
     set-wallpaper.sh -r
-elif [[ "${MONITORS}" == *"HDMI-1"* ]] ; then
+elif [[ "${MONITORS}" == *"DP-1-1"* ]] ; then
     # Home
-    xrandr --output HDMI-1 --mode 1920x1080 --pos 0x0 --rotate normal
+    xrandr --output DP-1-1 --mode 1920x1080 --pos 0x0 --rotate normal
     xrandr --output eDP-1 --primary --mode 1920x1080 --pos 0x1080 --rotate normal
     ~/.config/polybar/launch.sh
     set-wallpaper.sh -r
